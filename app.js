59f65eca2fc987fda25c2a78695f199a8216747e
habul17 <abulhassanca.dev@gmail.com>
2025-09-17 13:05:20 +0530

    Created child and siblings using react

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-const heading = React.createElement("h1", { id: 'heading' }, "Hello From React!");
+const parent = React.createElement('div',
+    { id: 'parent' },
+    [
+        React.createElement('div',
+            { id: 'child' },
+            [
+                React.createElement('h1', {}, 'I am an h1 tag'),
+                React.createElement('h2', {}, 'I am an h2 tag')
+            ]
+        ),
+        React.createElement('div',
+            { id: 'child' },
+            [
+                React.createElement('h1', {}, 'I am an h1 tag'),
+                React.createElement('h2', {}, 'I am an h2 tag')
+            ]
+        )
+    ]
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(heading);
+root.render(parent);
