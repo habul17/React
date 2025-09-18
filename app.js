5d9dfd6661a4b0d4c1f0b13b80e34df67fd3e2ba
habul17 <abulhassanca.dev@gmail.com>
2025-09-18 19:43:16 +0530

    Created Header

--- app.js
+++ app.js
@@ -2,8 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'
 
 
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='logo-container'>
+                <img
+                    className='logo'
+                    src='https://image.similarpng.com/file/similarpng/very-thumbnail/2021/07/Chef-restaurant-logo-illustrations-template-on-transparent-background-PNG.png'></img>
+            </div>
+            <div className='nav-items'>
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
 
+
+const AppLayout = () => {
+    return (
+        <div className='app'>
+            <Header />
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
