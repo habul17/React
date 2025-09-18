bb2be716785b5ac0bc88fb2b3f56397ec6897bdb
habul17 <abulhassanca.dev@gmail.com>
2025-09-18 13:54:40 +0530

    Created a component composition

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
 
-const jsxheading = <h1 className="head">From react</h1>
+const Title = () => (
+    <h1 className="Head" tabIndex="1">
+        Mastering-React 🚀
+    </h1>
+)
+
+
+const HeadingComponent = () => (
+    <div id='container'>
+        <Title />
+        <h1 className='heading'>This is from react component</h1>
+    </div>
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(jsxheading);
+root.render(<HeadingComponent />);
