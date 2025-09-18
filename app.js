8dc9f89fbe1d2eb53b981e3f8c19aeb0b5ab01aa
habul17 <abulhassanca.dev@gmail.com>
2025-09-18 21:54:17 +0530

    Added body component

--- app.js
+++ app.js
@@ -22,11 +22,46 @@ const Header = () => {
     )
 }
 
+const RestuarantCard = () => {
+    return (
+        <div className='res-card' style={{ backgroundColor: "#f0f0f0" }}>
+            <img
+                className='card-img' src='https://www.cubesnjuliennes.com/wp-content/uploads/2020/07/Chicken-Biryani-Recipe.jpg'></img>
+            <h3>HMR</h3>
+            <h4>Indian, Biryani</h4>
+            <h4>4.4 Stars</h4>
+            <h4>38 minutes</h4>
+        </div>
+    )
+}
+
+const Body = () => {
+    return (
+        <div className='body'>
+            <div className='search'>Search</div>
+            <div className='res-container'>
+                <RestuarantCard />
+                <RestuarantCard />
+                <RestuarantCard />
+                <RestuarantCard />
+                <RestuarantCard />
+                <RestuarantCard />
+                <RestuarantCard />
+                <RestuarantCard />
+                <RestuarantCard />
+                <RestuarantCard />
+                <RestuarantCard />
+            </div>
+        </div>
+    )
+}
+
 
 const AppLayout = () => {
     return (
         <div className='app'>
             <Header />
+            <Body />
         </div>
     )
 }
