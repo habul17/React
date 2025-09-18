267a8dbf58bed8886042cd808da95a3cf7c269ca
habul17 <abulhassanca.dev@gmail.com>
2025-09-18 19:24:57 +0530

    Creating a Restaurant app

--- app.js
+++ app.js
@@ -1,19 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
 
-const Title = () => (
-    <h1 className="Head" tabIndex="1">
-        Mastering-React 🚀
-    </h1>
-)
 
 
-const HeadingComponent = () => (
-    <div id='container'>
-        <Title />
-        <h1 className='heading'>This is from react component</h1>
-    </div>
-)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
