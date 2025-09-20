6b313d7824890354288037dfbbc76183188bbe0a
habul17 <abulhassanca.dev@gmail.com>
2025-09-20 10:28:17 +0530

    Real-Time Api integration

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
 import { LOGO_URL } from "../utils/contents";
+import { useState } from "react";
 
 const Header = () => {
+  const [btnName, setBtnName] = useState("Login");
+
   return (
     <div className="header">
       <div className="logo-container">
-        <img
-          className="logo"
-          src= {LOGO_URL}
-        ></img>
+        <img className="logo" src={LOGO_URL}></img>
       </div>
       <div className="nav-items">
         <ul>
@@ -15,6 +15,14 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button
+            className="login-btn"
+            onClick={() => {
+              btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
+            }}
+          >
+            {btnName}
+          </button>
         </ul>
       </div>
     </div>
